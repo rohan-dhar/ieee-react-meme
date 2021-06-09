00a296a34bf438e67bae2dc1df535a7c01364113
Rohan Dhar <rohan19443@iiitd.ac.in>
2021-06-09 07:39:02 +0530

    Update UI

--- src/components/MemeCard/index.jsx
+++ src/components/MemeCard/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "../CoreUI";
+
 import "./meme-card.css";
 
 const MemeCard = ({
@@ -16,29 +16,30 @@ const MemeCard = ({
 }) => {
 	return (
 		<section className="meme-card" {...props}>
-			<img src={url} alt={title} />
+			<main className="meme-card-hero">
+				<img src={url} alt={title} />
+			</main>
 			<main className="meme-card-content">
 				<h3>{title}</h3>
 				<footer className="meme-card-footer">
 					<section>
 						<b>{author}</b>
 					</section>
-					<section className="meme-card-footer-upvotes">
-						<b>{ups}</b> upvotes
-					</section>
 				</footer>
 				<footer className="meme-card-footer meme-card-footer-action">
-					<Button
-						small
-						green={!selected}
-						red={selected}
-						onClick={() => onMemeToggle(url)}
-					>
-						{" "}
-						{selected ? "Unselect" : "Select"}{" "}
-					</Button>
-					<a href={postUrl}>See in reddit</a>
+					<section className="meme-card-footer-upvotes">
+						<b>{ups}</b> 👍
+					</section>
+					<a href={postUrl}>See in reddit 👉</a>
 				</footer>
+				<button
+					className={`meme-card-select-button ${
+						selected ? "meme-card-select-button-selected" : ""
+					}`}
+					onClick={() => onMemeToggle(url)}
+				>
+					{selected ? "✅  Selected" : "Select"}
+				</button>
 			</main>
 		</section>
 	);
